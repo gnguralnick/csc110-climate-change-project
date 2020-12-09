ef8b45bc2ebc5b38bb65e4fb8d4af65bb5f6d698
mtoohey31 <mtoohey31@users.noreply.github.com>
2020-12-09 18:27:05 -0500

    Update temperature_import.py
    - Modified docstrings
    - Added dataframe import
    - Created helper function for processing years
    - Renamed functions to fit naming convention length

--- program/temperature_import.py
+++ program/temperature_import.py
@@ -6,11 +6,12 @@ from os import path
 from dataclasses import dataclass
 from typing import Dict
 import datetime
+import pandas as pd
 
 
 @dataclass
 class TemperatureData:
-    """A year of land-ocean temperature index data
+    """A year of land-ocean temperature index data.
 
     Instance Attributes:
         - year: the year that the data is from, stored as the first day from that year
@@ -22,15 +23,15 @@ class TemperatureData:
     smoothed: float
 
 
-def import_temperature_data_dict(filepath: str) -> Dict[datetime.date, TemperatureData]:
-    """Imports the temperature data from a csv file, and returns it as a dictionary of the first
-    day of the year of the data to the
+def import_as_dict(filepath: str) -> Dict[datetime.date, TemperatureData]:
+    """Import the temperature data from a csv file, and return it as a dictionary of the first
+    day of the year of the data to the data as a TemperatureData dataclass.
 
     Preconditions:
         - path.exists(filepath)
 
     >>> PATH = '../data/land-ocean_temperature_index/land-ocean_temperature_index.csv'
-    >>> import_temperature_data_dict(PATH)[datetime.date(1904, 1, 1)]
+    >>> import_as_dict(PATH)[datetime.date(1904, 1, 1)]
     TemperatureData(year=datetime.date(1904, 1, 1), raw=-0.47, smoothed=-0.3)
     """
     with open(filepath) as file:
@@ -39,16 +40,46 @@ def import_temperature_data_dict(filepath: str) -> Dict[datetime.date, Temperatu
         # Skip header row
         next(reader)
 
-        return {datetime.date(int(row[0]), 1, 1): TemperatureData(datetime.date(int(row[0]), 1, 1),
-                                                                  float(row[1]), float(row[2]))
+        return {first_date_in_year(row[0]): TemperatureData(first_date_in_year(row[0]),
+                                                            float(row[1]), float(row[2]))
                 for row in reader}
+
+
+def import_as_dataframe(filepath: str) -> pd.DataFrame:
+    """Return a dataframe representing the temperature data, containing the year as the first
+    day in the year, and the raw and smoothed data points.
+
+    Preconditions:
+        - path.exists(filepath)
+
+    >>> PATH = '../data/land-ocean_temperature_index/land-ocean_temperature_index.csv'
+    >>> df = import_as_dataframe(PATH)
+    >>> dict(df.iloc[88])
+    {'year': datetime.date(1968, 1, 1), 'raw': -0.08, 'smoothed': -0.03}
+    """
+    return pd.read_csv(filepath, header=0,
+                       names=['year', 'raw', 'smoothed'], converters={'year': first_date_in_year})
+
+
+def first_date_in_year(year: str) -> datetime.date:
+    """Return the first day in a given string integer year.
+
+    Preconditions:
+        - 0 < int(year) < 10000
+
+    >>> first_date_in_year('1')
+    datetime.date(1, 1, 1)
+    >>> first_date_in_year('2020')
+    datetime.date(2020, 1, 1)
+    """
+    return datetime.date(int(year), 1, 1)
 
 
 if __name__ == '__main__':
     import python_ta
     python_ta.check_all(config={
-        'extra-imports': ['csv', 'os', 'dataclasses', 'datetime', 'python_ta.contracts'],
-        'allowed-io': ['import_temperature_data_dict'],
+        'extra-imports': ['csv', 'os', 'dataclasses', 'datetime', 'python_ta.contracts', 'pandas'],
+        'allowed-io': ['import_as_dict'],
         'max-line-length': 100,
         'disable': ['R1705', 'C0200']
     })
