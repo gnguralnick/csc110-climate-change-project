653fbbcb9c65e993f3bd60e03933c60303f15eb3
Nathan <nathanhansenmail@gmail.com>
2020-12-10 14:29:25 -0500

    df_snow working

--- program/snowfall_import.py
+++ program/snowfall_import.py
@@ -1,17 +1,33 @@
 
-import pandas as ps
+from os import path
+import pandas as pd
+from dataclasses import dataclass
+import math
+from typing import List
 import python_ta
 
 
-def snowfall_csv_import(filepath: str) -> ps.DataFrame:
+def df_snow(filepath: str, parameters: List[str]) -> pd.DataFrame:
     """
-    This function returns a dataframe from the given csv file.
+    This function returns a dataframe containing the given parameters from the given csv file.
     Preconditions:
+        - path.exists(filepath)
+        -
+        - len(parameters) >= 1
 
-    Doctest:
+    (doctest goes here)
 
     """
-    return ps.read_csv(filepath)
+    df_import = pd.read_csv(filepath)
+
+    for col in df_import.columns:
+        if not any(col == prm for prm in parameters):
+            del df_import[col]
+
+    return df_import
+
+def aggregate_years(df: pd.DataFrame) -> None:
+    pass
 
 
 # python_ta.check_all(config={
