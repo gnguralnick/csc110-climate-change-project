34a3a443a3c7ad43b97202a4da2d91fcea8ffdad
Nathan <nathanhansenmail@gmail.com>
2020-12-10 14:26:56 -0500

    run function

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 """CSC110 Project -- US Snowfall vs. Global Land-Ocean Temperature Index"""
+import program
+from program import snowfall_import as si
+from program import temperature_import as ti
+from program import visualization as vis
+
+
 
 def run() -> None:
+    snow_params = ['Region', 'Start', 'End', 'RSI', 'Category']
+
+    snow_data = si.df_snow(
+        'data/snowfall/regional-snowfall-index_c20191218.csv',
+        snow_params
+        )
+    temp_data = ti.import_as_dataframe(
+        'data/land-ocean_temperature_index/land-ocean_temperature_index.csv'
+    )
+    vis.initialize_heatmap(temp_data, snow_data)
     while True:
         pass
