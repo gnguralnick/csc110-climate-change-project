93cce4e804f0b83fe7f0b12e644edec487e9d6cd
mtoohey31 <mtoohey31@users.noreply.github.com>
2020-12-11 09:21:36 -0500

    Simplified greatly to use pandas methods instead of the previous mess, which appeared to be working incorrectly as well

--- program/snowfall_import.py
+++ program/snowfall_import.py
@@ -35,45 +35,37 @@ def df_snow(filepath: str, parameters: List[str]) -> pd.DataFrame:
     return df_import
 
 
-def agg_years(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    This function aggregates the snowfall data into yearly averages for each region.
+# def agg_years_mean(df: pd.DataFrame) -> pd.DataFrame:
+#     """This function aggregates the snowfall data into yearly averages for each region.
+#
+#     Preconditions:
+#         - df.empty == False
+#         - "Region" in df.columns
+#         - "Year" in df.columns
+#         - "RSI" in df.columns
+#     """
+#     return df.groupby(['Year', 'Region'], as_index=False)[['RSI']].mean()
+
+
+def agg_years(df: pd.DataFrame, method: callable) -> pd.DataFrame:
+    """This function aggregates the snowfall data into yearly averages for each region.
+
     Preconditions:
         - df.empty == False
         - "Region" in df.columns
         - "Year" in df.columns
         - "RSI" in df.columns
     """
-    # agg_df = df.groupby(["Region", "Year"]).agg(['mean']).reset_index()
-    # agg_df['RSI'] = agg_df['RSI']['mean']
-    #
-    # return agg_df
-    regions = set(df['Region'])
-    years = set(df['Year'])
-    region_list = []
-    [region_list.extend(regions) for year in years]
-    year_list = []
-    [year_list.extend([year] * len(regions)) for year in years]
-    print(region_list)
-    print(year_list)
-    mean_list = [df.query('Year == ' + str(year) + ' and Region == "' + str(region) + '"')[
-                     'RSI'].mean()
-                 for region in regions for year in years]
-    print(mean_list)
-    return pd.DataFrame({'Region': region_list, 'Year': year_list, 'Mean RSI': mean_list})
+    return method(df.groupby(['Year', 'Region'], as_index=False)[['RSI']])
 
 
 def stateify_data(df: pd.DataFrame) -> pd.DataFrame:
     stateified_so_far = pd.DataFrame({'State': [], 'Year': [], 'Mean RSI': []})
-    # [[stateified = stateified.append(pd.DataFrame({'State': [state],
-    #                                                'Year': [df.iloc[index]['Year']],
-    #                                                'Mean RSI': [df.iloc[index]['Mean RSI']]}))
-    # for state in REGIONS[df.iloc[index]['Region']]] for index in range(len(df))]
     for index in range(len(df)):
         for state in REGIONS[df.iloc[index]['Region']]:
             stateified_so_far = stateified_so_far.append(
                 pd.DataFrame({'State': [state], 'Year': [df.iloc[index]['Year']],
-                              'Mean RSI': [df.iloc[index]['Mean RSI']]}))
+                              'RSI': [df.iloc[index]['RSI']]}))
     return stateified_so_far
 
 
